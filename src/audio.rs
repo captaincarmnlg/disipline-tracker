4bfad23cc9db176d5539b1d775080fb890eafda1
Nick K <captaincarmnlgames@gmail.com>
2025-09-29 16:22:20 +0200

    fixed audio async

--- src/audio.rs
+++ src/audio.rs
@@ -1,5 +1,6 @@
 use rodio::{Decoder, OutputStreamBuilder, Sink};
 use std::io::Cursor;
+use std::thread;
 
 const COMPLETE_SOUND: &[u8] = include_bytes!("../data/sounds/complete.ogg");
 
@@ -7,11 +8,13 @@ pub fn play_complete_sound() {
     let stream_handle =
         OutputStreamBuilder::open_default_stream().expect("open default audio stream");
 
-    let sink = Sink::connect_new(&stream_handle.mixer());
-    let cursor = Cursor::new(COMPLETE_SOUND);
-    let source = Decoder::try_from(cursor).unwrap();
-    sink.append(source);
-    sink.detach();
+    thread::spawn(move || {
+        let sink = Sink::connect_new(&stream_handle.mixer());
+        let cursor = Cursor::new(COMPLETE_SOUND);
+        let source = Decoder::try_from(cursor).unwrap();
+        sink.append(source);
+        sink.sleep_until_end();
+    });
 }
 
 #[test]
